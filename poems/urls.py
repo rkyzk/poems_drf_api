e04b67add794daa2ba41ed2b8400c791c213ac20
rkyzk <reikoyazaki@yahoo.com>
2023-06-03 13:26:33 +0000

    add poem detail view, allowing owner to edit/delete their poems

--- poems/urls.py
+++ poems/urls.py
@@ -3,4 +3,5 @@ from poems import views
 
 urlpatterns = [
     path('poems/', views.PoemList.as_view()),
+    path('poems/<int:pk>', views.PoemDetail.as_view()),
 ]
--- poems/views.py
+++ poems/views.py
@@ -1,8 +1,10 @@
+from django.http import Http404
 from rest_framework import status, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Poem
 from .serializers import PoemSerializer
+from poems_drf_api.permissions import IsOwnerOrReadOnly
 
 
 class PoemList(APIView):
@@ -29,4 +31,44 @@ class PoemList(APIView):
             )
         return Response(
             serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
+
+
+class PoemDetail(APIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = PoemSerializer
+
+    def get_object(self, pk):
+        """handle the case in which poem with the given id doesn't exit."""
+        try:
+            poem = Poem.objects.get(pk=pk)
+            self.check_object_permissions(self.request, poem)
+            return poem
+        except Poem.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        poem = self.get_object(pk)
+        serializer = PoemSerializer(
+            poem, context={'request': request}
+        )
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        poem = self.get_object(pk)
+        serializer = PoemSerializer(
+            poem, data=request.data, context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(
+            serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
+
+    def delete(self, request, pk):
+        poem = self.get_object(pk)
+        poem.delete()
+        return Response(
+            status=status.HTTP_204_NO_CONTENT
         )
