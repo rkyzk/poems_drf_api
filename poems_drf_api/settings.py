7c8def56575c4d88934a01883537642502c88045
rkyzk <reikoyazaki@yahoo.com>
2023-06-03 11:41:16 +0000

    add profile detail view, edit profile function and permissions, allowing only the owner to edit

--- poems_drf_api/settings.py
+++ poems_drf_api/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'secret_key'
+SECRET_KEY = 'django-insecure-1as!#1o_vpk_nhlp5ykly4d90)$g02pjb$&sx46^*43@z$pnxx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
