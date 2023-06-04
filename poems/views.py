9ba6db962399fbe6c66200a2929cb7f7f75ee3a6
rkyzk <reikoyazaki@yahoo.com>
2023-06-04 06:38:56 +0000

    add search filters to poems and profiles

--- poems/views.py
+++ poems/views.py
@@ -1,5 +1,6 @@
 from django.db.models import Count
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from poems_drf_api.permissions import IsOwnerOrReadOnly
 from .models import Poem
 from .serializers import PoemSerializer
@@ -12,16 +13,27 @@ class PoemList(generics.ListCreateAPIView):
     serializer_class = PoemSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Poem.objects.annotate(
-        likes_count=Count('like', distinct=True),
+        likes_count=Count('likes', distinct=True),
         comments_count=Count('comment', distinct=True)
     ).order_by('-created_at')
     filter_backends = [
-        filters.OrderingFilter
+        filters.OrderingFilter,
+        filters.SearchFilter,
+        DjangoFilterBackend,
+    ]
+    filterset_fields = [
+        'owner__followed__owner__profile',
+        'likes__owner__profile',
+        'owner__profile',
+    ]
+    search_fields = [
+        'owner__username',
+        'title',
     ]
     ordering_fields = [
         'likes_count',
         'comments_count',
-        'like_created_at',
+        'likes__created_at',
     ]
 
     def perform_create(self, serializer):
@@ -35,6 +47,6 @@ class PoemDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsOwnerOrReadOnly]
     serializer_class = PoemSerializer
     queryset = Poem.objects.annotate(
-        likes_count=Count('like', distinct=True),
+        likes_count=Count('likes', distinct=True),
         comments_count=Count('comment', distinct=True)
     ).order_by('-created_at')
