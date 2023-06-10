f5525514e094d3b1a64a6e9f02bce9e006bf1944
rkyzk <reikoyazaki@yahoo.com>
2023-06-10 06:30:32 +0000

    correct settings.py

--- poems_drf_api/settings.py
+++ poems_drf_api/settings.py
@@ -50,7 +50,8 @@ JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
 JWT_AUTH_SAMESITE = 'None'
 
 REST_AUTH_SERIALIZERS = {
-    'USER_DETAILS_SERIALIZER': 'drf_api.serializers.CurrentUserSerializer'
+    'USER_DETAILS_SERIALIZER':
+        'poems_drf_api.serializers.CurrentUserSerializer'
 }
 
 # Quick-start development settings - unsuitable for production
